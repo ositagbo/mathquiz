5d5d2ed259156a0feac58f97a4529d709a048cab
ositagbo <ositagbo@yahoo.com>
2025-10-10 21:41:01 +0200

    WIP

--- src/main/java/com/example/mathquiz/adapter/in/web/AuthenticationController.java
+++ src/main/java/com/example/mathquiz/adapter/in/web/AuthenticationController.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
 public class AuthenticationController {
